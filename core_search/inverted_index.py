3f36cc0f55a0b55febe34d463db65fb906c26b1e
Ranit Dey <you@example.com>
2020-06-24 23:58:35 +0530

    added few utils in string preprocessing and inverted index

--- core_search/inverted_index.py
+++ core_search/inverted_index.py
@@ -2,28 +2,29 @@
 Author: Ranit Dey
 Description: Inverted index data structure
 """
-
-import re
+from core_search.matcher.preprocessor import *
 
 
 class InvertedIndex:
-    pattern = re.compile('[\W_]+')
 
     def __init__(self):
         self.container = {}
 
-    def add_token(self,token,document_id):
-        token = self.make_alphanumeric(token)
+    def add_token(self, token, document_id):
+        token = make_alphanumeric(token)
         if token in self.container:
             self.container[token].add(document_id)
         else:
             self.container[token] = [document_id]
-
-    def make_alphanumeric(self,target):
-        return InvertedIndex.pattern.sub("", target)
 
     def get_matching_documents(self,token):
         if token in self.container:
             return self.container[token]
         else:
             return []
+
+    def load(self, data):
+        for ele in data:
+            processed_data = tokenize(cleanup_punctuation(ele["summary"]).lower())
+            for token in processed_data:
+                self.add_token(token, ele["id"])
