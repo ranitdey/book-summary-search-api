b311d2642bf9cf33dc62460c91b464d7436a227d
Ranit Dey <you@example.com>
2020-06-27 00:47:09 +0530

    Added search utility unit tedts

---
+++ core_search/test/data/search_data.py
@@ -0,0 +1,46 @@
+TEST_DATA_SEARCH = [
+    {
+      "id": 30,
+      "query": "The Book in Three Sentences: Hosting dinners with like-minded people is one of the most powerful way to build fantastic relationships in business and in life. You should think carefully about who you invite to these meals and look for uncommon commonalities that make it more likely the guests will resonate with one another. Be the gatekeeper of your network and assume responsibility for the people you surround yourself with in life."
+    },
+    {
+      "id": 31,
+      "query": "The Book in Three Sentences:\u00a0The most successful path to mastering anything is to practice for the sake of the practice itself, not for the result. All significant learning is composed of brief spurts of progress followed by long periods of work where if feels as if you are stuck on a plateau.\u00a0There are no experts\u2013only learners."
+    },
+    {
+      "id": 32,
+      "query": "The Book in Three Sentences:\u00a0You can\u2019t do the same things others do and expect to outperform. The most dependable way to outperform the market is to buy something for less than its value. It is price, not quality that determines value: high-quality assets can be risky, and low-quality assets can be safe."
+    },
+    {
+      "id": 33,
+      "query": "The Book in Three Sentences: It is more important to live fully than to live in a straight line. The surest gauge of the impact a life makes is how many other lives it touches. Nothing in life looks the same once you truly understand that you are not exempt from death."
+    },
+    {
+      "id": 34,
+      "query": "The Book in Three Sentences: The two classic drivers of human development are nature (genes) and nurture (environment). Many people mistakenly believe nurture only refers to how parents raise their children. Although children do learn things from their parents, they learn far more from their peers. The world that children share with their peer group is what shapes their behavior, modifies the characteristics they were born with, and determines the sort of people they will be when they grow up."
+    },
+    {
+      "id": 35,
+      "query": "The Book in Three Sentences: Oliver Sacks was a brilliant physician and a fantastic writer. He lived a full life that included dealing with criticism over being gay, attending medical school at Oxford University, experimenting with heavy drug use, traveling the United States and Canada by motorcycle, suffering life-threatening injuries, squatting a California state record of 600 pounds, and being honored by the Queen of England for his many books and storied career as a physician. Sacks is a symbol of the importance of writing, the power of exploration and inquisitiveness, and the need for empathy."
+    },
+    {
+      "id": 36,
+      "query": "The Book in Three Sentences:\u00a0We all fear death, but life is long if you know how to use it. Putting things off is the biggest waste of life: it snatches away each day as it comes, and denies us the present by promising the future: live immediately.\u00a0In any situation in life you will find delights and relaxations and pleasures if you are prepared to make light of your troubles and not let them distress you."
+    },
+    {
+      "id": 37,
+      "query": "The Book in Three Sentences: The human brain is wired for inattention and inertia. As a result, many people already have good intentions, but don\u2019t follow through due to forgetfulness, procrastination, or a general lack of awareness. We can bridge the gap between our intentions and our behavior by using strategies to lock in our future behavior like active choice, pre-commitment, good design, reframing, and simplicity."
+    },
+    {
+      "id": 38,
+      "query": "The Book in Three Sentences: All of life is practice in one form or another. Actively practicing something is very different from passively learning. You will never reach a level of performance that feels complete, so learn to love the art of practicing your skill."
+    },
+    {
+      "id": 39,
+      "query": "The Book in Three Sentences: Before you pay your expenses, take your profit first. Run your business based on what you can afford to do today, not what you hope to be able to afford someday. When profit comes first, it is the focus, and it is never forgotten."
+    },
+    {
+      "id": 40,
+      "query": "The Book in Three Sentences: This book is an autobiography written by a 13-year-old boy from Japan about what it is like to live with autism. The way autistic people view the world is very different than the way we may perceive them to view the world. This disconnect between how we view and treat people with autism and how they actually view the world makes living with autism even more difficult."
+    }
+]
---
+++ core_search/test/search_tests.py
@@ -0,0 +1,26 @@
+from core_search.test.data.search_data import TEST_DATA_SEARCH
+from core_search.search import Search
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+
+def test001_search_validation_exact_match():
+    mock_data = TEST_DATA_SEARCH
+    search = Search()
+    for data in mock_data:
+        LOGGER.info("Processing query for id %s" % data["id"])
+        result = search.search_summaries(data["query"], 4)
+        assert len(result) == 4
+        assert result[0]["id"] == data["id"]
+
+
+def test002_search_validation_partial_match():
+    mock_data = TEST_DATA_SEARCH
+    search = Search()
+    for data in mock_data:
+        LOGGER.info("Processing query for id %s" % data["id"])
+        query = data["query"]
+        result = search.search_summaries(query[0:len(query)//2], 4)
+        assert len(result) == 4
+        assert result[0]["id"] == data["id"]
